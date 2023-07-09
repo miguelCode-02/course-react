4ad04a8fd9789e86f51ba2dee1aa40e1a5fd20bd
miguelCode-02 <miguelpoter22@gmail.com>
2023-07-09 09:23:04 -0500

    first class

---
+++ projects/00-hola-mundo/src/App.jsx
@@ -0,0 +1,38 @@
+import './App.css'
+import { TwitterFollowCard } from './TwitterFollowCard.jsx'
+
+const users = [
+  {
+    userName: 'midudev',
+    name: 'Miguel Ángel Durán',
+    isFollowing: true
+  },
+  {
+    userName: 'miguelCode-02',
+    name: 'Miguel Arcangel Lopez Coneo',
+    isFollowing: true
+  },
+  {
+    userName: 'TMChein',
+    name: 'Tomas',
+    isFollowing: false
+  }
+]
+
+export function App () {
+  return (
+    <section className='App'>
+      {
+        users.map(({ userName, name, isFollowing }) => (
+          <TwitterFollowCard
+            key={userName}
+            userName={userName}
+            initialIsFollowing={isFollowing}
+          >
+            {name}
+          </TwitterFollowCard>
+        ))
+      }
+    </section>
+  )
+}
