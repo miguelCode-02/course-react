a0dc8d813c8340014dbaba074cfd082b8c483861
Miguel Arcangel <miguelpoter22@gmail.com>
2023-08-05 20:47:53 -0500

    code refactoring using slinter

--- projects/02-tic-tac-toe/src/App.jsx
+++ projects/02-tic-tac-toe/src/App.jsx
@@ -9,87 +9,83 @@ import { WinnerModal } from './components/WinnerModal.jsx'
 import { BuildBoard } from './components/BuildBoard.jsx'
 import { saveGameToStorage, resetGameStorage, saveWinnerToStorage } from './logic/storage'
 
+function App () {
+  const [board, setBoard] = useState(() => {
+    const boardFromStorage = window.localStorage.getItem('board')
+    return boardFromStorage ? JSON.parse(boardFromStorage) : Array(9).fill(null)
+  })
 
-function App() {
+  const [turn, setTurn] = useState(() => {
+    const turnFromStorage = window.localStorage.getItem('turn')
+    return turnFromStorage ?? TURNS.X
+  })
 
-	const [board, setBoard] = useState(() => {
-		const boardFromStorage = window.localStorage.getItem('board')
-		return boardFromStorage ? JSON.parse(boardFromStorage) : Array(9).fill(null)
-	})
+  const [winner, setWinner] = useState(() => {
+    const winnerFromStorage = window.localStorage.getItem('winner')
+    return winnerFromStorage || null
+  })
 
-	const [turn, setTurn] = useState(() => {
-		const turnFromStorage = window.localStorage.getItem('turn')
-		return turnFromStorage ?? TURNS.X
-	})
+  const [showBoard, setShowBoard] = useState(false)
 
-	const [winner, setWinner] = useState(() => {
-		const winnerFromStorage = window.localStorage.getItem('winner')
-		return winnerFromStorage ? winnerFromStorage : null
-	})
+  const updateBoard = index => {
+    if (board[index] || winner) return
 
-	const [showBoard, setShowBoard] = useState(false)
+    const newBoard = [...board]
+    newBoard[index] = turn
+    setBoard(newBoard)
 
-	const updateBoard = index => {
-		if (board[index] || winner ) return
+    const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X
+    setTurn(newTurn)
 
-		const newBoard = [...board]
-		newBoard[index] = turn
-		setBoard(newBoard)
+    saveGameToStorage({ board: newBoard, turn: newTurn })
 
-		const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X
-		setTurn(newTurn)
+    const newWinner = checkWinner(newBoard)
+    if (newWinner) {
+      setWinner(newWinner)
+      setShowBoard(true)
+      confetti()
+      saveWinnerToStorage(newWinner)
+    } else if (checkEndGame(newBoard)) {
+      setWinner(false)
+      setShowBoard(true)
+      confetti()
+    }
+  }
 
-		saveGameToStorage({board : newBoard, turn: newTurn})
+  const resetGame = () => {
+    // ? se cambia todos los estados para empezar el juego de nuevo
+    setShowBoard(false)
+    setBoard(Array(9).fill(null))
+    setTurn(TURNS.X)
+    setWinner(null)
+    resetGameStorage()
+  }
 
-		const newWinner = checkWinner(newBoard)
-		if (newWinner) {
-			setWinner(newWinner)
-			setShowBoard(true)
-			confetti()
-			saveWinnerToStorage(newWinner)
-		}else if(checkEndGame(newBoard)){
-			setWinner(false)
-			setShowBoard(true)
-			confetti()
-		}
+  const showSolvedBoard = () => {
+    setShowBoard(false)
+  }
 
-	}
+  const showGameResult = () => {
+    setShowBoard(true)
+  }
 
-	const resetGame = () => {
-		//? se cambia todos los estados para empezar el juego de nuevo
-		setShowBoard(false)
-		setBoard(Array(9).fill(null))
-		setTurn(TURNS.X)
-		setWinner(null)
-		resetGameStorage()
-	}
+  return (
+    <main className='board'>
+      <h1>Tic Tac Toe</h1>
+      <button onClick={resetGame}>Empezar de nuevo</button>
+      {!showBoard && winner && <button onClick={showGameResult}>Mostrar ganador</button>}
+      <section className='game'>
+        <BuildBoard board={board} updateBoard={updateBoard} />
+      </section>
 
-	const showSolvedBoard = () => {
-		setShowBoard(false)
-	}
+      <section className='turn'>
+        <Squares isSelected={turn === TURNS.X}>{TURNS.X}</Squares>
+        <Squares isSelected={turn === TURNS.O}>{TURNS.O}</Squares>
+      </section>
 
-	const showGameResult = () => {
-		setShowBoard(true)
-
-	}
-
-	return (
-		<main className='board'>
-			<h1>Tic Tac Toe</h1>
-			<button onClick={resetGame}>Empezar de nuevo</button>
-			{!showBoard && winner  && <button onClick={showGameResult}>Mostrar ganador</button>}
-			<section className='game'>
-				<BuildBoard board={board} updateBoard={updateBoard} />
-			</section>
-
-			<section className='turn'>
-				<Squares isSelected={turn === TURNS.X}>{TURNS.X}</Squares>
-				<Squares isSelected={turn === TURNS.O}>{TURNS.O}</Squares>
-			</section>
-
-			<WinnerModal resetGame={resetGame} showSolvedBoard={showSolvedBoard} winner={winner} showBoard={showBoard}/>
-		</main>
-	)
+      <WinnerModal resetGame={resetGame} showSolvedBoard={showSolvedBoard} winner={winner} showBoard={showBoard} />
+    </main>
+  )
 }
 
 export default App
