b94637bee97908933b9c251c0046a9f60de26089
Miguel Arcangel <miguelpoter22@gmail.com>
2023-12-22 15:56:54 -0500

    Add language selection functionality and reducer

--- projects/08-google-translate-clone/src/App.tsx
+++ projects/08-google-translate-clone/src/App.tsx
@@ -1,9 +1,72 @@
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './App.css'
+import { useReducer } from 'react'
+import { type State, type Action } from './types'
+
+const initialState: State = {
+  fromLanguage: 'auto',
+  toLanguage: 'en',
+  fromText: '',
+  result: '',
+  loading: false
+}
+
+function reducer(state: State, action: Action) {
+  const { type } = action
+
+  if (type === 'INTERCHANGE_LANGUAGES') {
+    return {
+      ...state,
+      fromLanguage: state.toLanguage,
+      toLanguage: state.fromLanguage
+    }
+  }
+
+  if (type === 'SET_FROM_LANGUAGE') {
+    return {
+      ...state,
+      fromLanguage: action.payload
+    }
+  }
+
+  if (type === 'SET_TO_LANGUAGE') {
+    return {
+      ...state,
+      toLanguage: action.payload
+    }
+  }
+
+  if (type === 'SET_FROM_TEXT') {
+    return {
+      ...state,
+      loading: true,
+      fromText: action.payload,
+      result: ''
+    }
+  }
+
+  if (type === 'SET_RESULT') {
+    return {
+      ...state,
+      loading: false,
+      result: action.payload
+    }
+  }
+
+  return state
+}
+
 
 function App() {
+  const [{ fromLanguage }, dispatch] = useReducer(reducer, initialState)
+
+  console.log({ fromLanguage })
+
   return (
     <div className='App'>
+      <button onClick={() => {
+        dispatch({ type: 'SET_FROM_LANGUAGE', payload: 'es' })
+      }} >Cambiar a español</button>
       <h1>Google Translate</h1>
     </div>
   )
